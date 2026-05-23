f6948437a3649bea4dbb6b9c10feaeb88ad48ead
yesung <kimyesung0215@gmail.com>
2026-05-23 08:34:05 +0000

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/team-scheduler/',
+  base: '/team_scheduler/',
   plugins: [
     react(),
     VitePWA({
